3fcffc2d6bcab5f309a1a2a09d8d448968bf3bc0
Brendan Dolan-Gavitt <daniel13520cs@gmail.com>
2018-02-22 19:11:17 -0500

    fix miniet script file path

--- lab2/script-mininet-customize-topo-lab2.sh
+++ lab2/script-mininet-customize-topo-lab2.sh
@@ -1,4 +1,4 @@
 #!/bash/bin
 
 echo "setup lab2 partI topology..\n"
-sudo mn --custom ~/mininet/custom/topo-lab2-part1.py --topo mytopo
+sudo mn --custom ~/mininet/custom/lab2/topo-lab2-part1.py --topo mytopo
